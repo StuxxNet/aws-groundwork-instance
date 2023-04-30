8c5f2e2abfaf11bac48c0abf432b4ce7ede4cf7d
Ramon Silveira Borges <ramonboorges@gmail.com>
2023-04-30 14:24:20 +0200

    first commit

---
+++ groundwork.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  # Configuration options
+  region = "eu-central-1"
+}
+
+module "aws_groundwork_vpc" {
+  source = "git::https://github.com/StuxxNet/iac-aws-groundwork.git?ref=main"
+}
+
+output "module_vpc_id" {
+  value = module.aws_groundwork_vpc.vpc_id
+}
+
+output "module_public_subnets" {
+  value = module.aws_groundwork_vpc.public_subnets_data
+}
+
+output "module_private_subnets" {
+  value = module.aws_groundwork_vpc.private_subnets_data
+}
